a06e0b7a06683fe9150f6116432d7af23bff7bf2
An <antp9254@gmail.com>
2022-07-21 21:11:08 +0700

    Update machine_rent.c

--- DSA Lab/midterm/midterm 2/machine_rent.c
+++ DSA Lab/midterm/midterm 2/machine_rent.c
@@ -2,14 +2,21 @@
 #include<stdlib.h>
 #include<string.h>
 
+#define max(a,b) ((a)>(b)?(a):(b))
 
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
 
 int main()
 {   
     int n;
     int *s, *e, *c;
     scanf("%d", &n);
-    int ans =0;
+    int ans =-1;
     s = (int *)malloc(sizeof(int)*n);
     e = (int *)malloc(sizeof(int)*n);
     c = (int *)malloc(sizeof(int)*n);
@@ -19,17 +26,35 @@ int main()
     }
     for (int i=0; i<n; i++)
     {
-        for (int j=i; j<n; j++)
+        for (int j=i+1; j<n; j++)
         {
-            if (s[i] == e[j] || e[i] == s[j])
+            if (s[i] > s[j])
             {
-                continue;
-            } else
-            {
-                ans += c[i];
+                swap(&s[i], &s[j]);
+                swap(&e[i], &e[j]);
+                swap(&c[i], &c[j]);
             }
         }
     }
-    printf("%d", 60);
+    int index =0;
+    while (index < n)
+    {
+        int start = s[index];
+        int end = e[index];
+        int cost = c[index];
+        int total_cost = cost;
+    
+        for (int i=index+1; i<n; i++)
+        {
+            if (s[i] > end)
+            {
+                total_cost += c[i];
+                end = e[i];
+            }
+        }
+        ans = max(ans, total_cost);
+        index++;
+    }  
+    printf("%d\n", ans);
     return 0;
 }
